98e8a795860c33c25eefef44e87294884f1943f7
Jurgens Banninga <jurgens@interexcel.co.za>
2015-08-21 11:19:06 +0200

    Add new fields

--- app/payments/sagepaynow.php
+++ app/payments/sagepaynow.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * sagepaynow.php
- * 
+ *
  * All in one script for submitting the form and handling the callback
- * 
+ *
  */
 use Tygh\Http;
 use Tygh\Registry;
@@ -14,11 +14,11 @@ if (! defined ( 'BOOTSTRAP' )) {
 
 if (empty ( $processor_data )) {
 	$order_id = $_POST ['Reference'];
-	
+
 	// Get actual order ID from unique ID
 	$pieces = explode("_", $order_id);
 	$order_id = $pieces[0];
-	
+
 	$order_info = fn_get_order_info ( $order_id );
 	$processor_data = fn_get_processor_data ( $order_info ['payment_id'] );
 }
@@ -35,7 +35,7 @@ $sagepaynow_service_key = $processor_data ['processor_params'] ['service_key'];
 // Scroll the bottom to see form submit code
 if (defined ( 'PAYMENT_NOTIFICATION' )) {
 	if ($mode == 'notify' && ! empty ( $_REQUEST ['order_id'] )) {
-		
+
 		if (fn_check_payment_script ( 'sagepaynow.php', $_POST ['Reference'], $processor_data )) {
 			$pp_response = array ();
 			$sagepaynow_statuses = $processor_data ['processor_params'] ['statuses'];
@@ -46,79 +46,79 @@ if (defined ( 'PAYMENT_NOTIFICATION' )) {
 			$pnData = array ();
 			$pnParamString = '';
 			pnlog ( 'Sage Pay Now IPN call received' );
-			
+
 			// // Notify Sage Pay Now that information has been received
 			if (! $pnError && ! $pnDone) {
 				header ( 'HTTP/1.0 200 OK' );
 				flush ();
 			}
-			
+
 			// // Get data sent by Sage Pay Now
 			if (! $pnError && ! $pnDone) {
 				pnlog ( 'Get posted data' );
-				
+
 				// Posted variables from IPN
 				$pnData = pnGetData ();
-				
+
 				pnlog ( 'Sage Pay Now Data: ' . print_r ( $pnData, true ) );
-				
+
 				if ($pnData === false) {
 					$pnError = true;
 					$pnErrMsg = PN_ERR_BAD_ACCESS;
 				}
 			}
-			
+
 			// Get internal cart
 			if (! $pnError && ! $pnDone) {
 				// TODO The line below shows too much data
 				// pnlog ( "Purchase:\n" . print_r ( $order_info, true ) );
 			}
-			
+
 			pnlog ( "Checking data against internal order..." );
-			
+
 			// Check data against internal order
 			if (! $pnError && ! $pnDone) {
 				// pnlog( 'Check data against internal order' );
-				
+
 				// Check order amount
 				if (! pnAmountsEqual ( $pnData ['Amount'], fn_format_price ( $order_info ['total'], $processor_data ['processor_params'] ['currency'] ) )) {
 					$pnError = true;
 					$pnErrMsg = PN_ERR_AMOUNT_MISMATCH;
 				}
 			}
-			
+
 			pnlog ( "Finished checking data against internal order." );
-			
+
 			// // Check status and update order
 			if (! $pnError && ! $pnDone) {
 				pnlog ( 'Checking status and update order' );
-				
+
 				$transaction_id = $pnData ['Reference'];
-				
+
 				switch ($pnData ['TransactionAccepted']) {
 					case 'true' :
 						pnlog ( '- Complete' );
 						$pp_response ['order_status'] = $sagepaynow_statuses ['completed'];
-						break;					
+						break;
 					case 'false':
-                        pnlog( '- Failed' );                       
-                        $pp_response['order_status'] = $sagepaynow_statuses['denied'];        
+                        pnlog( '- Failed' );
+                        $pp_response['order_status'] = $sagepaynow_statuses['denied'];
                         break;
 					// TODO Remove not used section
 					//case 'false' :
 // 					case "PENDING" :
 // 						pnlog ( '- Pending' );
 // 						$pp_response ['order_status'] = $sagepaynow_statuses ['pending'];
-// 						break;					
+// 						break;
 					default :
 						// If unknown status, do nothing (safest course of action)
 						break;
 				}
-				
+
 				$pp_response ['reason_text'] = $pnData ['Reason'];
 				$pp_response ['transaction_id'] = $transaction_id;
 				$pp_response ['customer_email'] = $pnData ['email_address'];
-				
+
 				// TODO Re-evaluate this code for efficiency, clean up failed/pending duplicate
 				if ($pp_response ['order_status'] == $sagepaynow_statuses ['denied']) {
 					fn_change_order_status ( $order_id, $pp_response ['order_status'] );
@@ -136,18 +136,18 @@ if (defined ( 'PAYMENT_NOTIFICATION' )) {
 		pnlog("Mode == return");
 		if (fn_check_payment_script ( 'sagepaynow.php', $_REQUEST ['order_id'] )) {
 			$order_info = fn_get_order_info ( $_REQUEST ['order_id'], true );
-			
+
 			if ($order_info ['status'] == STATUS_INCOMPLETED_ORDER) {
 				fn_change_order_status ( $_REQUEST ['order_id'], 'O', '', false );
 			}
-			
+
 			if (fn_allowed_for ( 'MULTIVENDOR' )) {
 				if ($order_info ['status'] == STATUS_PARENT_ORDER) {
 					$child_orders = db_get_hash_single_array ( "SELECT order_id, status FROM ?:orders WHERE parent_order_id = ?i", array (
 							'order_id',
-							'status' 
+							'status'
 					), $_REQUEST ['order_id'] );
-					
+
 					foreach ( $child_orders as $order_id => $order_status ) {
 						if ($order_status == STATUS_INCOMPLETED_ORDER) {
 							fn_change_order_status ( $order_id, 'O', '', false );
@@ -162,12 +162,12 @@ if (defined ( 'PAYMENT_NOTIFICATION' )) {
 		pnlog("Mode == cancel");
 		$order_info = fn_get_order_info ( $_REQUEST ['order_id'] );
 		$Reason = $_REQUEST['Reason'];
-		
+
 		$pp_response ['order_status'] = 'N';
 		//$pp_response ["reason_text"] = __ ( 'text_transaction_cancelled' );
 		$pp_response ["reason_text"] = $Reason;
 		pnlog("Reason for transaction failure:" . $Reason);
-		
+
 		fn_finish_payment ( $_REQUEST ['order_id'], $pp_response, false );
 		fn_order_placement_routines ( 'route', $_REQUEST ['order_id'] );
 	}
@@ -175,20 +175,24 @@ if (defined ( 'PAYMENT_NOTIFICATION' )) {
 	// The form is about to be submitted to Sage Pay Now
 	$total = fn_format_price ( $order_info ['total'], $processor_data ['processor_params'] ['currency'] );
 	$m_payment_id = $order_info ['order_id'];
-	
+
 	// Create an unique order ID
 	$m_payment_id = $m_payment_id . "_" . date("Ymds");
-	
+
 	$return_url = fn_url ( "payment_notification.return?payment=sagepaynow&order_id=$m_payment_id", AREA, 'current' );
 	$cancel_url = fn_url ( "payment_notification.cancel?payment=sagepaynow&order_id=$m_payment_id", AREA, 'current' );
 	$notify_url = fn_url ( "payment_notification.notify?payment=sagepaynow&order_id=$m_payment_id", AREA, 'current' );
-	
+
 	$callback_url = "dispatch=payment_notification.notify&payment=sagepaynow&order_id=$m_payment_id";
-	
+
+	$customerName = "{$order_info['b_firstname']} {$order_info['b_lastname']}";
+	$orderID = $order_info['order_id'];
+	$customerID = $order_info['user_id']; // TODO: Not sure if this customer ID is correct..
+	$sageGUID = "TBC";
+
 	$payArray = array (
 			'm1' => $sagepaynow_service_key,
 			'm2' => '24ade73c-98cf-47b3-99be-cc7b867b3080',
-			'm4' => $return_url,
 			'm5' => $cancel_url,
 			// 'm6' => $notify_url,
 			'm10' => $callback_url,
@@ -199,27 +203,31 @@ if (defined ( 'PAYMENT_NOTIFICATION' )) {
 			'p4' => $total,
             // 18 Aug '14 modifed P3
             // 'p3' => __ ( 'text_sagepaynow_item_name' ) . ' - ' . $order_info ['order_id'],
-			'p3' => __ ( 'text_sagepaynow_item_name' ) . ' (' . $order_info ['b_firstname'] . ' ' . $order_info ['b_lastname'] . ' - Order #' . $order_info ['order_id'] . ')',
+
             'm6' => __ ( 'text_sagepaynow_item_name' ) . ' (' . $order_info ['b_firstname'] . ' ' . $order_info ['b_lastname'] . ' - Order #' . $order_info ['order_id'] . ')',
-			'description' => __ ( 'total_product_cost' ) 
+			'description' => __ ( 'total_product_cost' ),
+
+			'p3' => "{$customerName} | {$orderID}",
+			'm3' => "$sageGUID",
+			'm4' => "{$customerID}",
 	);
-	
+
 	$inputs = '';
 	foreach ( $payArray as $k => $v ) {
 		$inputs .= "<input type='hidden' name='$k' value='$v' />\n";
 	}
-	
+
 	$msg = fn_get_lang_var ( 'text_cc_processor_connection' );
 	$msg = str_replace ( '[processor]', 'Sage Pay Now', $msg );
-	
+
 	pnlog ( "payArray: " . print_r ( $payArray, true ) );
-	
+
 	echo <<<EOT
     <html>
     <body onLoad="document.sagepaynow_form.submit();">
     <form action="https://paynow.sagepay.co.za/site/paynow.aspx" method="post" name="sagepaynow_form">
     $inputs
-   
+
     </form>
     <div align=center>{$msg}</div>
     </body>
